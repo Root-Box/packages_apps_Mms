0a47e645386399bb79df9431607ee2a5a2547641
detule <ogjoneski@gmail.com>
2012-12-20 22:34:29 -0500

    Mms: Enforce thumbnail rotation when needed
    Prior Behavior: ThumbnailManager will bail on bitmap operations if no
    re-scaling is needed.
    
    New Behavior: ThumbnailManager will bail on bitmap operations if no re-scaling
    AND no orientation adjustment is needed.
    
    Change-Id: Ib5f49ce9fcd5f981407cac5a33bb8e77fdf13a01

--- src/com/android/mms/util/ThumbnailManager.java
+++ src/com/android/mms/util/ThumbnailManager.java
@@ -29,6 +29,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Matrix;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.util.Log;
@@ -310,6 +311,8 @@ public class ThumbnailManager extends BackgroundLoaderManager {
 
             UriImage uriImage = new UriImage(mContext, mUri);
             String path = uriImage.getPath();
+            int orientation = uriImage.getOrientation();
+            Log.w(TAG, "Orientation requested for thumbnail = " + orientation);
 
             if (path == null) {
                 return null;
@@ -333,6 +336,11 @@ public class ThumbnailManager extends BackgroundLoaderManager {
                 if (bitmap == null) {
                     Log.w(TAG, "decode cached failed " + path);
                 }
+                if (orientation != 0) {
+                    Matrix m = new Matrix();
+                    UriImage.endowTransformMatrix(m, 1, orientation);
+                    bitmap = Bitmap.createBitmap(bitmap,0, 0, bitmap.getWidth(), bitmap.getHeight(), m, false);
+                }
                 return bitmap;
             } else {
                 Bitmap bitmap;
@@ -346,7 +354,7 @@ public class ThumbnailManager extends BackgroundLoaderManager {
                     return null;
                 }
 
-                bitmap = resizeDownBySideLength(bitmap, THUMBNAIL_TARGET_SIZE, true);
+                bitmap = resizeDownBySideLength(bitmap, THUMBNAIL_TARGET_SIZE, orientation, true);
 
                 if (!isTempFile) {
                     byte[] array = compressBitmap(bitmap);
@@ -389,27 +397,30 @@ public class ThumbnailManager extends BackgroundLoaderManager {
                     BitmapFactory.decodeByteArray(bytes, offset, length, options));
         }
 
+        // @param orientation: After resizing also rotate
         private Bitmap resizeDownBySideLength(
-                Bitmap bitmap, int maxLength, boolean recycle) {
+                Bitmap bitmap, int maxLength, int orientation, boolean recycle) {
             int srcWidth = bitmap.getWidth();
             int srcHeight = bitmap.getHeight();
             float scale = Math.min(
                     (float) maxLength / srcWidth, (float) maxLength / srcHeight);
-            if (scale >= 1.0f) return bitmap;
-            return resizeBitmapByScale(bitmap, scale, recycle);
+            if (scale >= 1.0f && orientation == 0) return bitmap;
+
+            Log.w(TAG, "resizeDownBySideLength, orientation = " + orientation);
+            return resizeBitmapByScale(bitmap, Math.min(scale, 1.0f), orientation, recycle);
         }
 
+        // @param orientation: After resizing also rotate
         private Bitmap resizeBitmapByScale(
-                Bitmap bitmap, float scale, boolean recycle) {
+                Bitmap bitmap, float scale, int orientation, boolean recycle) {
+            Matrix m = new Matrix();
+            UriImage.endowTransformMatrix(m,scale,orientation);
             int width = Math.round(bitmap.getWidth() * scale);
             int height = Math.round(bitmap.getHeight() * scale);
             if (width == bitmap.getWidth()
-                    && height == bitmap.getHeight()) return bitmap;
-            Bitmap target = Bitmap.createBitmap(width, height, getConfig(bitmap));
-            Canvas canvas = new Canvas(target);
-            canvas.scale(scale, scale);
-            Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG);
-            canvas.drawBitmap(bitmap, 0, 0, paint);
+                    && height == bitmap.getHeight() && orientation ==0) return bitmap;
+            Log.w(TAG, "resizeBitmapByScale, orientation = " + orientation + " scale = " + scale);
+            Bitmap target = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),m,false);
             if (recycle) bitmap.recycle();
             return target;
         }
@@ -516,7 +527,7 @@ public class ThumbnailManager extends BackgroundLoaderManager {
             float scale = Math.max(
                     (float) targetSize / srcWidth, (float) targetSize / srcHeight);
             if (scale > 0.5f) return bitmap;
-            return resizeBitmapByScale(bitmap, scale, recycle);
+            return resizeBitmapByScale(bitmap, scale, 0, recycle);
         }
 
     }
