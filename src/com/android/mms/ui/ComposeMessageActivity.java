0eb0babebbb433a8f001a1a764039f31beb4659f
mnazim <mnm9994u@gmail.com>
2012-12-29 12:51:07 -0700

    Add attachment save location preference
    Change-Id: I300368fdd7c3420b11d4d105fc245b2342cd33dd

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -149,6 +149,7 @@ import com.android.mms.templates.TemplatesProvider.Template;
 import com.android.mms.transaction.MessagingNotification;
 import com.android.mms.ui.MessageListView.OnSizeChangedListener;
 import com.android.mms.ui.MessageUtils.ResizeImageResultCallback;
+import com.android.mms.ui.MessagingPreferenceActivity;
 import com.android.mms.ui.RecipientsEditor.RecipientContextMenuInfo;
 import com.android.mms.util.DraftCache;
 import com.android.mms.util.EmojiParser;
@@ -1717,7 +1718,7 @@ public class ComposeMessageActivity extends Activity
     }
 
     /**
-     * Copies media from an Mms to the "download" directory on the SD card. If any of the parts
+     * Copies media from an Mms to a directory on the SD card. If any of the parts
      * are audio types, drm'd or not, they're copied to the "Ringtones" directory.
      * @param msgId
      */
@@ -1790,9 +1791,15 @@ public class ComposeMessageActivity extends Activity
                 // Depending on the location, there may be an
                 // extension already on the name or not. If we've got audio, put the attachment
                 // in the Ringtones directory.
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String mMmsDir = prefs.getString(MessagingPreferenceActivity.MMS_SAVE_LOCATION, "download");
                 String dir = Environment.getExternalStorageDirectory() + "/"
                                 + (ContentType.isAudioType(type) ? Environment.DIRECTORY_RINGTONES :
                                     Environment.DIRECTORY_DOWNLOADS)  + "/";
+                if (!mMmsDir.isEmpty()) {
+                    dir = Environment.getExternalStorageDirectory() + "/"
+                                    + prefs.getString(MessagingPreferenceActivity.MMS_SAVE_LOCATION, "download")  + "/";
+                }
                 String extension;
                 int index;
                 if ((index = fileName.lastIndexOf('.')) == -1) {
--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -67,6 +67,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
     public static final String GROUP_MMS_MODE           = "pref_key_mms_group_mms";
+    public static final String MMS_SAVE_LOCATION        = "pref_save_location";
 
     // Emoji and Unicode
     public static final String ENABLE_EMOJIS             = "pref_key_enable_emojis";
